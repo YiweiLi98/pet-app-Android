b22724e31c948e0a05d3eda01ca573943a8e6324
YiweiLi98 <100244641+YiweiLi98@users.noreply.github.com>
2023-03-29 19:51:08 +1100

    Add files via upload

---
+++ project/app/build/generated/ap_generated_sources/debug/out/com/bumptech/glide/GeneratedAppGlideModuleImpl.java
@@ -0,0 +1,51 @@
+package com.bumptech.glide;
+
+import android.content.Context;
+import android.util.Log;
+import androidx.annotation.NonNull;
+import com.bumptech.glide.integration.okhttp3.OkHttpLibraryGlideModule;
+import comp5216.sydney.edu.au.finalproject.MyGlideAppModule;
+import java.util.Collections;
+import java.util.Set;
+
+@SuppressWarnings("deprecation")
+final class GeneratedAppGlideModuleImpl extends GeneratedAppGlideModule {
+  private final MyGlideAppModule appGlideModule;
+
+  public GeneratedAppGlideModuleImpl(Context context) {
+    appGlideModule = new MyGlideAppModule();
+    if (Log.isLoggable("Glide", Log.DEBUG)) {
+      Log.d("Glide", "Discovered AppGlideModule from annotation: comp5216.sydney.edu.au.finalproject.MyGlideAppModule");
+      Log.d("Glide", "Discovered LibraryGlideModule from annotation: com.bumptech.glide.integration.okhttp3.OkHttpLibraryGlideModule");
+    }
+  }
+
+  @Override
+  public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
+    appGlideModule.applyOptions(context, builder);
+  }
+
+  @Override
+  public void registerComponents(@NonNull Context context, @NonNull Glide glide,
+      @NonNull Registry registry) {
+    new OkHttpLibraryGlideModule().registerComponents(context, glide, registry);
+    appGlideModule.registerComponents(context, glide, registry);
+  }
+
+  @Override
+  public boolean isManifestParsingEnabled() {
+    return appGlideModule.isManifestParsingEnabled();
+  }
+
+  @Override
+  @NonNull
+  public Set<Class<?>> getExcludedModuleClasses() {
+    return Collections.emptySet();
+  }
+
+  @Override
+  @NonNull
+  GeneratedRequestManagerFactory getRequestManagerFactory() {
+    return new GeneratedRequestManagerFactory();
+  }
+}
---
+++ project/app/build/generated/source/buildConfig/debug/comp5216/sydney/edu/au/finalproject/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package comp5216.sydney.edu.au.finalproject;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "comp5216.sydney.edu.au.finalproject";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
